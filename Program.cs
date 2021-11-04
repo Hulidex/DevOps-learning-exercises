f7f72ffe3097bcf5a44bbec13ab89f922521ac26
Jose Luis Izquierdo <Hulidex@users.noreply.github.com>
2021-11-04 19:43:05 +0100

    Initial commit
    - Sample .NET core 5.0 console app
    - Basic Jenkins File

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_console_app_for_testing_jenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
